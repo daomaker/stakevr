b0b1f727869beb42cd89711de074a4e80af0d7e6
iHux3 <ihux3x@gmail.com>
2021-12-13 01:25:02 +0100

    init

---
+++ contracts/StakeVR.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "hardhat/console.sol";
+
+contract StakeVR is ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
+    uint32 constant HUNDRED_PERCENT = 1e3;
+
+    struct Stake {
+        bool unstaked;
+        uint128 amount;
+        uint48 lockTimestamp;
+        uint16 lockDays;
+    }
+
+    IERC20 public stakingToken;
+    mapping(address => Stake[]) public stakers;
+    uint192 public totalShares;
+    uint16 public minLockDays;
+    uint16 public maxLockDays;
+    uint16 public shareBonusPerYear;
+    uint16 public shareBonusPer1MTokens;
+
+    event EStake(
+        address staker,
+        uint128 amount,
+        uint192 shares,
+        uint48 lockTimestamp,
+        uint16 lockDays
+    );
+
+    event EUnstake(
+        address staker,
+        uint stakeIndex
+    );
+
+    constructor(
+        IERC20 _stakingToken,
+        uint16 _minLockDays,
+        uint16 _maxLockDays,
+        uint16 _shareBonusPerYear,
+        uint16 _shareBonusPer1MTokens
+    ) {
+        require(address(_stakingToken) != address(0));
+        require(_minLockDays <= _maxLockDays, "StakeVR: minLockDays > maxLockDays");
+        stakingToken = _stakingToken;
+        minLockDays = _minLockDays;
+        maxLockDays = _maxLockDays;
+        shareBonusPerYear = _shareBonusPerYear;
+        shareBonusPer1MTokens = _shareBonusPer1MTokens;
+    }
+
+    function stake(uint128 amount, uint16 lockDays) external nonReentrant {
+        require(lockDays >= minLockDays && lockDays <= maxLockDays, "StakeVR: invalid lockDays");
+        (uint192 shares,,) = calculateShares(amount, lockDays);
+        totalShares += shares;
+
+        stakers[msg.sender].push(Stake(
+            false,
+            amount,
+            uint48(block.timestamp),
+            lockDays
+        ));
+        stakingToken.safeTransferFrom(msg.sender, address(this), amount);
+        emit EStake(msg.sender, amount, shares, uint48(block.timestamp), lockDays);
+    }
+
+    function unstake(uint stakeIndex) external nonReentrant {
+        require(stakeIndex < stakers[msg.sender].length, "StakeVR: invalid index");
+        Stake storage stakeRef = stakers[msg.sender][stakeIndex];
+        require(!stakeRef.unstaked, "StakeVR: unstaked already");
+        require(stakeRef.lockTimestamp + uint48(stakeRef.lockDays) * 86400 <= block.timestamp, "StakeVR: unstaking too early");
+
+        (uint192 shares,,) = calculateShares(stakeRef.amount, stakeRef.lockDays);
+        totalShares -= shares;
+        stakeRef.unstaked = true;
+        stakingToken.safeTransfer(msg.sender, stakeRef.amount);
+        emit EUnstake(msg.sender, stakeIndex);
+    }
+
+    function calculateShares(
+        uint amount, 
+        uint lockDays
+    ) public view returns (
+        uint192 shares,
+        uint longTermBonus,
+        uint stakingMoreBonus
+    ) {
+        longTermBonus = lockDays * shareBonusPerYear / 365;
+        stakingMoreBonus = amount * shareBonusPer1MTokens / 1e24;
+        shares = uint192(amount + amount * (longTermBonus + stakingMoreBonus) / HUNDRED_PERCENT);
+    }
+
+    function getStakerInfo(
+        address stakerAddress
+    ) public view returns (
+        uint256 totalStakeAmount,
+        uint256 totalStakerShares
+    ) {
+        for (uint i = 0; i < stakers[stakerAddress].length; i++) {
+            Stake storage stakeRef = stakers[stakerAddress][i];
+            if (stakeRef.unstaked) continue;
+
+            totalStakeAmount += stakeRef.amount;
+            (uint192 shares,,) = calculateShares(stakeRef.amount, stakeRef.lockDays);
+            totalStakerShares += shares;
+        }
+    }
+
+    function stakerStakeCount(address stakerAddress) public view returns (uint) {
+        return stakers[stakerAddress].length;
+    }
+}
---
+++ test/test.js
@@ -0,0 +1,170 @@
+const { expect } = require("chai");
+const { time } = require("@openzeppelin/test-helpers");
+
+describe("StakeVR contract", function() {
+    let deployer, user1, user2, user3, contract, stakingToken, stakingTokenDecimals;
+
+    const PRECISION_LOSS = "10000000000000000";
+    
+    const parseUnits = (value, decimals = stakingTokenDecimals) => {
+        return ethers.utils.parseUnits(value.toString(), decimals);
+    }
+
+    const init = async(minLockDays, maxLockDays, stakeBonusLong, stakeBonusMore, stakingTokenDecimals = 18) => {
+        const ERC20Mock = await ethers.getContractFactory("ERC20Mock");
+        stakingToken = await ERC20Mock.deploy("MOCK1", "MOCK1", deployer.address, parseUnits(1e12), stakingTokenDecimals);
+
+        const Contract = await ethers.getContractFactory("StakeVR");
+        contract = await Contract.deploy(
+            stakingToken.address,
+            minLockDays,
+            maxLockDays,
+            stakeBonusLong,
+            stakeBonusMore
+        );
+
+        expect(await contract.stakingToken()).to.equal(stakingToken.address);
+        expect(await contract.minLockDays()).to.equal(minLockDays);
+        expect(await contract.maxLockDays()).to.equal(maxLockDays);
+        expect(await contract.shareBonusPerYear()).to.equal(stakeBonusLong);
+        expect(await contract.shareBonusPer1MTokens()).to.equal(stakeBonusMore);
+
+        await stakingToken.transfer(user1.address, parseUnits(1e11));
+        await stakingToken.transfer(user2.address, parseUnits(1e11));
+        await stakingToken.transfer(user3.address, parseUnits(1e11));
+
+        stakingToken = stakingToken.connect(user1);
+        await stakingToken.approve(contract.address, parseUnits(1e11));
+        stakingToken = stakingToken.connect(user2);
+        await stakingToken.approve(contract.address, parseUnits(1e11));
+        stakingToken = stakingToken.connect(user3);
+        await stakingToken.approve(contract.address, parseUnits(1e11));
+    }
+
+    const stake = async(user, amount, lockDays, expectedShares, expectedLongTermBonus, expectedStakingMoreBonus) => {
+        contract = contract.connect(user);
+        amount = parseUnits(amount);
+        expectedShares = parseUnits(expectedShares);
+        
+        const totalSharesBefore = await contract.totalShares();
+        const contractBalanceBefore = await stakingToken.balanceOf(contract.address);
+        await contract.stake(amount, lockDays);
+        const totalSharesAfter = await contract.totalShares();
+        const contractBalanceAfter = await stakingToken.balanceOf(contract.address);
+
+        const calculateSharesResult = await contract.calculateShares(amount, lockDays);
+        expect(calculateSharesResult[0]).to.closeTo(expectedShares, PRECISION_LOSS);
+        expect(calculateSharesResult[1]).to.equal(expectedLongTermBonus);
+        expect(calculateSharesResult[2]).to.equal(expectedStakingMoreBonus);
+        expect(totalSharesAfter).to.equal(totalSharesBefore.add(calculateSharesResult[0]));
+        expect(contractBalanceAfter).to.equal(contractBalanceBefore.add(amount));
+
+        const timeNow = Number(await time.latest());
+        const stakeIndex = Number(await contract.stakerStakeCount(user.address)) - 1;
+        const stakeInfo = await contract.stakers(user.address, stakeIndex);
+        expect(stakeInfo.unstaked).to.equal(false);
+        expect(stakeInfo.amount).to.equal(amount);
+        expect(stakeInfo.lockTimestamp).to.equal(timeNow);
+        expect(stakeInfo.lockDays).to.equal(lockDays);
+    }
+
+    const unstake = async(user, stakeIndex, tooEarly, unstakedAlready) => {
+        contract = contract.connect(user);
+
+        if (tooEarly) {
+            await expect(contract.unstake(stakeIndex)).to.be.revertedWith("StakeVR: unstaking too early");
+            return;
+        }
+
+        if (unstakedAlready) {
+            await expect(contract.unstake(stakeIndex)).to.be.revertedWith("StakeVR: unstaked already");
+            return;
+        }
+
+        const stakeInfoBefore = await contract.stakers(user.address, stakeIndex);
+        const calculateSharesResult = await contract.calculateShares(stakeInfoBefore.amount, stakeInfoBefore.lockDays);
+
+        const totalSharesBefore = await contract.totalShares();
+        const contractBalanceBefore = await stakingToken.balanceOf(contract.address);
+        const stakerBalanceBefore = await stakingToken.balanceOf(user.address);
+        await contract.unstake(stakeIndex);
+        const totalSharesAfter = await contract.totalShares();
+        const contractBalanceAfter = await stakingToken.balanceOf(contract.address);
+        const stakerBalanceAfter = await stakingToken.balanceOf(user.address);
+
+        expect(totalSharesBefore).to.equal(totalSharesAfter.add(calculateSharesResult[0]));
+        expect(contractBalanceBefore).to.equal(contractBalanceAfter.add(stakeInfoBefore.amount));
+        expect(stakerBalanceAfter).to.equal(stakerBalanceBefore.add(stakeInfoBefore.amount));
+
+        const stakeInfoAfter = await contract.stakers(user.address, stakeIndex);
+        expect(stakeInfoAfter.unstaked).to.equal(true);
+    }
+
+    before(async () => {
+        [deployer, user1, user2, user3] = await ethers.getSigners();
+    });
+
+    describe("Full flow test", async() => {
+        before(async() => {
+            await init(
+                30,
+                3650,
+                1000,
+                1000
+            );
+        });
+
+        it("min max lock days", async() => {
+            contract = contract.connect(user1);
+            await expect(contract.stake(parseUnits(1), 1)).to.be.revertedWith("StakeVR: invalid lockDays");
+            await expect(contract.stake(parseUnits(1), 3651)).to.be.revertedWith("StakeVR: invalid lockDays");
+        });
+        
+        it("staking", async() => {
+            await stake(user1, 1, 30, 1.08, 82, 0);
+            await stake(user1, 1, 365, 2, 1000, 0);
+            await stake(user1, 10, 365, 20, 1000, 0);
+            await stake(user1, 1000, 365, 2001, 1000, 1);
+
+            const stakerInfo = await contract.getStakerInfo(user1.address);
+            expect(stakerInfo[0]).to.equal(parseUnits(1012));
+            expect(stakerInfo[1]).to.equal(parseUnits(2024.082));
+
+            await stake(user2, 1000000, 365, 3000000, 1000, 1000);
+            await stake(user2, 100, 730, 300, 2000, 0);
+            await stake(user3, 100, 3650, 1100, 10000, 0);
+            await stake(user3, 1000000000, 30, 1001082000000, 82, 1000000);
+        });
+
+        it("unstaking", async() => {
+            await expect(contract.unstake(100)).to.be.revertedWith("StakeVR: invalid index");
+
+            await time.increase(time.duration.days(29));
+            await unstake(user1, 0, true, false);
+            await time.increase(time.duration.days(1));
+            await unstake(user1, 0, false, false);
+            await unstake(user1, 0, false, true);
+
+            await time.increase(time.duration.days(365 - 31));
+            await unstake(user1, 1, true, false);
+            await time.increase(time.duration.days(1));
+            await unstake(user1, 1, false, false);
+            await unstake(user1, 1, false, true);
+
+            const stakerInfo = await contract.getStakerInfo(user1.address);
+            expect(stakerInfo[0]).to.equal(parseUnits(1010));
+            expect(stakerInfo[1]).to.equal(parseUnits(2021));
+
+            await unstake(user2, 0, false, false);
+            await unstake(user2, 1, true, false);
+            await unstake(user3, 1, false, false);
+
+            await time.increase(time.duration.days(3650));
+            await unstake(user2, 1, false, false);
+            await unstake(user3, 1, false, true);
+            await unstake(user3, 0, false, false);
+
+            await stake(user2, 10000, 365, 20100, 1000, 10);
+        });
+    });
+});
